4e6765b2f3c3ec06d8a838ca0834bfcc2984f431
Niko Musubine <nikochan252525252357@gmail.com>
2022-12-13 21:30:38 +0900

    Helpコマンド作成

---
+++ CmdParser.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenJTalkInterfaceForYMM4
+{
+    public static class CmdParser
+    {
+        public static string[]? ParseCmdArgs(string[] args)
+        {
+            List<string> strs = new List<string>();
+
+            StringBuilder strbuild = new StringBuilder();
+            
+            foreach(string s in args)
+            {
+                strbuild.Append(s);
+                strbuild.Append(" ");
+            }
+
+            string str = strbuild.ToString();
+
+            byte situation = 0;
+            //0: none 1: single quotation 2: double quotation
+
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i != str.Length; i++)
+            {
+                switch (str.Substring(i))
+                {
+                    case "\'":
+                        if (situation == 0)
+                        {
+                            situation = 1;
+                        }
+
+                        else if (situation == 1)
+                        {
+                            situation = 0;
+                        }
+                        else if (situation == 2)
+                        {
+                            sb.Append("\'");
+                        }
+                        break;
+                    case "\"":
+                        break;
+                    case " ":
+                        break;
+                }
+            }
+        } 
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,7 +10,33 @@ namespace OpenJTalkInterfaceForYMM4
     {
         static void Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                return;
+            }
+            else
+            {
+                if (args[0].ToLower() == "help")
+                {
+                    System.Reflection.Assembly myAssembly =   System.Reflection.Assembly.GetExecutingAssembly();
+                    System.IO.StreamReader sr =
+                        new System.IO.StreamReader(
+                        myAssembly.GetManifestResourceStream("OpenJTalkInterfaceForYMM4.Help.txt"),
+                            System.Text.Encoding.UTF8);
+                    Console.WriteLine(sr.ReadToEnd());
+                    return;
+                }
 
+                StringBuilder sb = new StringBuilder();
+                
+                for(int i = 0; i < args.Length; i++)
+                {
+                    switch (args[i])
+                    {
+                    }
+                }
+
+            }
         }
     }
 }
